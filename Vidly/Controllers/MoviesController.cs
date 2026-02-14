cf27fddc2969f277e081213399cab97d362d98aa
Saurav Bhattacharya <online.saurav@gmail.com>
2026-02-13 19:36:01 -0800

    fix: add year range constraint to ByReleaseDate route - restricts year to 1888-2100

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -68,7 +68,7 @@ namespace Vidly.Controllers
             return RedirectToAction("Index");
         }
 
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        [Route("movies/released/{year:range(1888,2100)}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
